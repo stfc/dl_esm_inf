ab15562704239d3a905693147fe6003af3d966a2
Andrew Porter <andrew.porter@stfc.ac.uk>
2018-04-27 14:11:13 +0100

    #5 add new parallel_mod

--- finite_difference/src/field_mod.f90
+++ finite_difference/src/field_mod.f90
@@ -1,9 +1,39 @@
+!------------------------------------------------------------------------------
+! BSD 2-Clause License
+! 
+! Copyright (c) 2017-2018, Science and Technology Facilities Council
+! All rights reserved.
+! 
+! Redistribution and use in source and binary forms, with or without
+! modification, are permitted provided that the following conditions are met:
+! 
+! * Redistributions of source code must retain the above copyright notice, this
+!   list of conditions and the following disclaimer.
+! 
+! * Redistributions in binary form must reproduce the above copyright notice,
+!   this list of conditions and the following disclaimer in the documentation
+!   and/or other materials provided with the distribution.
+! 
+! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+! AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+! IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+! DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+! FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+! DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+! SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+! OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+! OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+!------------------------------------------------------------------------------
+! Author: A. R. Porter, STFC Daresbury Laboratory
+
 module field_mod
   use kind_params_mod
   use region_mod
   use halo_mod
   use grid_mod
   use gocean_mod, only: gocean_stop
+  use tile_mod
   implicit none
 
   private
@@ -16,18 +46,6 @@ module field_mod
   integer, public, parameter :: F_POINTS   = 3
   !> A field that lives on all grid-points of the grid
   integer, public, parameter :: ALL_POINTS = 4
-
-  !> A field is sub-divided into tiles for coarse-grained OpenMP
-  type :: tile_type
-     !> Tile region to use when only a field's 'internal'
-     !! points are required
-     type(region_type) :: internal
-     !> Tile region to use when all of a field's points are required
-     type(region_type) :: whole
-     ! Could potentially physically divide up an array into 
-     ! distinct tiles and store the data for each separately...
-     !real(wp), dimension(:,:), allocatable :: data
-  end type tile_type
 
   !> The base field type. Intended to represent a global field
   !! such as the x-component of velocity.
