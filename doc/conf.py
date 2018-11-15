99c8a86893fdca71f77dcf2629d76a71ebb1d5b1
Andrew Porter <andrew.porter@stfc.ac.uk>
2018-11-15 15:27:33 +0000

    #14 rm use of sphinx-fortran for the minute

--- doc/conf.py
+++ doc/conf.py
@@ -33,7 +33,7 @@ release = '1.0'
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-# needs_sphinx = '1.0'
+# needs_sphinx = '1.6.7'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -41,7 +41,6 @@ release = '1.0'
 extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
-    'sphinxfortran',
 ]
 
 fortran_source = ["../finite_difference/src/*.f90"]
