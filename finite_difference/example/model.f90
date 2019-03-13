7aee798aa5ea7b46081f684295ccd7f1682c16e8
Andrew Porter <andrew.porter@stfc.ac.uk>
2019-03-13 15:35:15 +0000

    #7 example: add code to dump fields and check halo values

--- finite_difference/example/model.f90
+++ finite_difference/example/model.f90
@@ -88,10 +88,10 @@ program model
   t_field = r2d_field(model_grid, GO_T_POINTS)
   f_field = r2d_field(model_grid, GO_F_POINTS)
 
-  call init_field_hill(u_field, model_grid)
-  call init_field_hill(v_field, model_grid)
-  call init_field_hill(t_field, model_grid)
-  call init_field_hill(f_field, model_grid)
+  call init_field_hill(u_field)
+  call init_field_hill(v_field)
+  call init_field_hill(t_field)
+  call init_field_hill(f_field)
 
   write(*,*) "Halo exchange for u:"
   call u_field%halo_exch(1)
@@ -100,6 +100,12 @@ program model
   !call t_field%halo_exch(1)
   !call f_field%halo_exch(1)
 
+  call dump_field(u_field, "u_fld", halo_depth=1)
+  call dump_field(v_field, "v_fld")
+
+  ! Check the halo regions
+  call check_hill_halos(u_field)
+  
   ! All done!
   if (my_rank == 1) write(*,'(/"Example model set-up complete."/)')
 
@@ -118,16 +124,101 @@ contains
     
   end subroutine init_field_by_rank
 
-  subroutine init_field_hill(field, grid)
+  subroutine init_field_hill(field)
     type(r2d_field), intent(inout) :: field
-    type(grid_type), intent(inout) :: grid
     integer :: ji, jj
+    integer :: istart, istop, jstart, jstop
+    istart = field%internal%xstart
+    istop =  field%internal%xstop
+    jstart = field%internal%ystart
+    jstop =  field%internal%ystop
+    
     field%data(:,:) = 0.0
-    do jj = field%internal%ystart, field%internal%ystop
-       do ji = field%internal%xstart, field%internal%xstop
-          field%data(ji,jj) = field%grid%subdomain%global%xstart + ji + &
-               field%grid%subdomain%global%ystart + jj
+    do jj = jstart, jstop
+       do ji = istart, istop
+          field%data(ji,jj) = hill(field%grid%subdomain%global%xstart +ji-1 , &
+                                   field%grid%subdomain%global%ystart +jj-1)
        end do
     end do
+    ! Set external points to plausible but wrong values
+    do jj = 1, field%grid%ny
+       field%data(1:istart-1,jj) = field%data(istart,jj)
+       field%data(istop+1:,jj) = field%data(istop,jj)
+    end do
+    do ji = 1, field%grid%nx
+       field%data(ji,1:jstart-1) = field%data(ji,jstart)
+       field%data(ji,jstop+1:) = field%data(ji,jstop)
+    end do
   end subroutine init_field_hill
+
+  function hill(ji, jj)
+    integer, intent(in) :: ji, jj
+    real(go_wp) :: hill
+    hill = real(ji + jj)
+  end function hill
+  
+  subroutine check_hill_halos(field)
+    type(r2d_field), intent(in) :: field
+    ! Locals
+    integer :: ji, jj, iglobal, jglobal
+    real(go_wp), parameter :: TOL_ZERO = 1.0E-8
+
+    if(field%grid%subdomain%global%xstart > 1)then
+       ! This subdomain has a halo region in the -x direction
+       ji = field%internal%xstart - 1
+       do jj = field%internal%ystart, field%internal%ystop
+          iglobal = field%grid%subdomain%global%xstart + ji - 1
+          jglobal = field%grid%subdomain%global%ystart + jj - 1 
+          if( abs(field%data(ji, jj) - hill(iglobal, jglobal)) > TOL_ZERO)then
+             write(*,*) "ERROR in halo value: ", ji, jj, iglobal, jglobal, &
+                  field%data(ji,jj), hill(iglobal,jglobal)
+          end if
+       end do
+    end if
+    
+  end subroutine check_hill_halos
+  
+  subroutine dump_field(field, name, halo_depth)
+    use parallel_mod, only: get_rank
+    type(r2d_field), intent(in) :: field
+    character(len=*), intent(in) :: name
+    integer, intent(in), optional :: halo_depth
+    ! Locals
+    integer, parameter :: unit_no = 21
+    integer :: ji, jj
+    character(len=5) :: rank_str
+    integer :: lhalo
+
+    ! Each rank creates its own file
+    my_rank = get_rank()
+    write(rank_str, "(I5.5)") my_rank
+    open(unit=unit_no, file=name//"_"//TRIM(rank_str)//".dat", &
+         status="unknown", action="write")
+
+    if(present(halo_depth))then
+       lhalo = halo_depth
+       if(lhalo >= field%internal%xstart .or. &
+            lhalo >= field%internal%ystart)then
+          write (*,*) "field_dump: error: requested halo depth is &
+               & greater than the halo depth of supplied field - not &
+               & writing data"
+          return
+       end if
+    else
+       lhalo = 0
+    end if
+    
+    do jj = field%internal%ystart-lhalo, field%internal%ystop+lhalo, 1
+       do ji = field%internal%xstart-lhalo, field%internal%xstop+lhalo, 1
+
+          write(unit_no,'(3e16.7e3)') field%grid%xt(ji,jj), &
+                                      field%grid%yt(ji,jj), &
+                                      field%data(ji,jj)
+       end do
+       ! Blank line at end of scan for gnuplot
+       write(unit_no,*)
+    end do
+    close(unit_no)
+       
+  end subroutine dump_field
 end program model
